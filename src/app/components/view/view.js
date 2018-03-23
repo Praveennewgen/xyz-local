0dcb46a473fad7d9179b255cb123c57f860fd77d
Praveen Srivastava <praveen.srivastava@newgen.co.in>
2018-03-23 17:28:58 +0530

    Success and error model UI

--- src/app/components/view/view.js
+++ src/app/components/view/view.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular.module('iot').component('view', {
@@ -7,9 +7,9 @@
         templateUrl: 'app/components/view/view.html',
     });
 
-    viewLayoutController.$inject = ['$scope', '$http', '$stateParams', '$state'];
+    viewLayoutController.$inject = ['$scope', '$http', '$stateParams', '$state', '$timeout'];
 
-    function viewLayoutController($scope, $http, $stateParams, $state) {
+    function viewLayoutController($scope, $http, $stateParams, $state, $timeout) {
         var vm = this;
         /* togglePannel for open close side pannel*/
         vm.togglePannel = false;
@@ -18,6 +18,10 @@
         vm.disablePowerscout = 0;
         vm.disableSensor = 0;
         vm.simulateDisabled = true;
+        /* variable for simulate submit response*/
+        vm.smilulateScuccess = false;
+        vm.smilulateErr = false;
+        vm.submitResp = false;
 
         var layoutType = $stateParams.layoutType;
         var zipCode = '10001';
@@ -25,25 +29,39 @@
         vm.layoutDetails = null;
         vm.objPos = null;
 
+        //closeErrModel();
 
-        vm.changeLayout = function(selectedLayoutObj) {
-            $state.go('view', { layoutType: selectedLayoutObj.LayoutType });
+        function closeErrModel() {
+            vm.smilulateErr = true;
+            $timeout(function () {
+                vm.smilulateErr = false;
+            }, 5000);
+        }
+        vm.close_popup = function () {
+            vm.submitResp = false;
+        }
+        vm.changeLayout = function (selectedLayoutObj) {
+            $state.go('view', {
+                layoutType: selectedLayoutObj.LayoutType
+            });
         }
 
-        vm.selectLayoutOption = function(layoutOption) {
+        vm.selectLayoutOption = function (layoutOption) {
             vm.selectedMenu = layoutOption;
         }
 
-        vm.selectedRangeOption = function(range) {
+        vm.selectedRangeOption = function (range) {
             vm.selectedRange = range;
         }
 
-        vm.evtSimulate = function() {
+        vm.evtSimulate = function () {
             var postObject = {};
             postObject.powerscouts = _.filter(vm.layoutDetails.powerscouts, areEnabled);
             postObject.sensors = _.filter(vm.layoutDetails.sensors, areEnabled);
             postObject.range = vm.selectedRange.id;
-            postObject.weather = { "ZipCode": zipCode };
+            postObject.weather = {
+                "ZipCode": zipCode
+            };
 
             console.log(postObject);
 
@@ -53,37 +71,37 @@
         }
 
         $http.get('./data/layoutDetails-' + layoutType + '.json', false)
-            .then(function(res) {
+            .then(function (res) {
                 vm.layoutDetails = res.data;
 
                 //vm.disablePowerscout = vm.layoutDetails.powerscouts.length;
                 //vm.disableSensor = vm.layoutDetails.sensors.length;
 
-            }, function(err) {
+            }, function (err) {
                 console.log("Error in fetching data: " + err);
             });
 
         $http.get('./data/chooseLayout.json', false)
-            .then(function(res) {
+            .then(function (res) {
                 updateLayoutDetails(res.data.data);
                 //  vm.showLoading=true;             
-            }, function(err) {
+            }, function (err) {
                 console.log("Error in fetching data from json: " + err);
                 // vm.showLoading=false;
             });
 
         $http.get('./data/iconCoordinates.json', false)
-            .then(function(res) {
+            .then(function (res) {
                 vm.objPos = res.data.positions;
-            }, function(err) {
+            }, function (err) {
                 console.log("Error in fetching data: " + err);
             });
 
         $http.get('./data/selectRange.json', false)
-            .then(function(res) {
+            .then(function (res) {
                 vm.range = res.data.range;
                 vm.selectedRange = vm.range[0];
-            }, function(err) {
+            }, function (err) {
                 console.log("Error in fetching data from json: " + err);
                 //vm.showLoading=false;     
             });
@@ -95,29 +113,35 @@
 
 
             var geocoder = new google.maps.Geocoder();
-            var win = function(position) {
+            var win = function (position) {
                 var mylat = position.coords.latitude;
                 var mylng = position.coords.longitude;
                 var latlng = new google.maps.LatLng(mylat, mylng);
-                geocoder.geocode({ 'latLng': latlng },
-                    function(results, status) {
+                geocoder.geocode({
+                        'latLng': latlng
+                    },
+                    function (results, status) {
                         if (status == google.maps.GeocoderStatus.OK) {
                             console.log("The user's zipcode is " + results[0].address_components[6].short_name);
                             zipCode = results[0].address_components[6].short_name;
                         }
                     });
             };
-            var fail = function(e) {
+            var fail = function (e) {
                 console.log("GPS Failed:" + e);
                 //return '10001';
             };
 
-            navigator.geolocation.getCurrentPosition(win, fail, { enableHighAccuracy: true, maximumAge: 600000, timeout: 10000 });
+            navigator.geolocation.getCurrentPosition(win, fail, {
+                enableHighAccuracy: true,
+                maximumAge: 600000,
+                timeout: 10000
+            });
         }
 
         function updateLayoutDetails(data) {
             vm.menuData = data;
-            data.forEach(function(item) {
+            data.forEach(function (item) {
                 if (item.LayoutType === layoutType) {
                     vm.selectedMenu = item;
                 }
